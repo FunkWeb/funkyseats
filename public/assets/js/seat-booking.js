a651a2bda7612c36c3a282c7e7b8ffc3be760799
irynaosoba <ir.osoba@gmail.com>
2021-10-18 09:51:32 +0200

    added custom css and js

---
+++ public/assets/js/seat-booking.js
@@ -0,0 +1,7 @@
+function bookSeat(){
+//     document.getElementsByClassName('bookingWindow')[0].style.display = 'flex';
+//     document.getElementsByClassName('bookingWindow')[0].style.width = '216px';
+//     document.getElementsByClassName('bookingWindow').style.transition = 'all 1s';
+
+    console.log(`${{ $seat_number }}`)
+}
