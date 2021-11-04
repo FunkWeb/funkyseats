5350f3c4e83154a3f0fae884da6ae2a41e119eed
irynaosoba <ir.osoba@gmail.com>
2021-11-04 10:45:58 +0100

    add check login to edit rooms/seats

--- resources/views/includes/component/room-editing.blade.php
+++ resources/views/includes/component/room-editing.blade.php
@@ -1,12 +1,16 @@
 <div class='text-center text-light text-uppercase mt-30px box-style'
     style="background-image: url({{ asset('images/office.jpeg') }})">
-    <div class="edit-box">
-        <i class="far fa-trash-alt" style="cursor: pointer" onclick="showWindow()"></i>
+    @if (Auth::check())
+    <div class="edit-box" style="pointer-events: all">
+    @else
+    <div class="edit-box" style="pointer-events: none">
+    @endif
+        <i class="far fa-trash-alt" style="cursor: pointer" onclick="showWindow({{$id}}, 'rooms')"></i>
         {{-- <a href="/room/{{ $id ?? '' }}/seats/edit" style='text-decoration: none;'> --}}
         <form action=/rooms/{{ $id ?? '' }}/save method="post">
             @csrf
             <input type="text" id="name" name="name" value={{ $name }}>
-            <button class='submit-changes-btn' type='submit' value='submit'> Update </button>
+            <button class='submit-changes-btn' type='submit' value='submit'>Update</button>
         </form>
         </a>
     </div>
--- resources/views/includes/component/seat-editing.blade.php
+++ resources/views/includes/component/seat-editing.blade.php
@@ -1,7 +1,11 @@
 <div class='text-center text-light text-uppercase mt-30px seat-container admin-seat-bg-color'
     style="background-image: url({{ asset('images/office.jpeg') }})">
-    <div>
-        <i class="far fa-trash-alt" style="cursor: pointer" onclick="showWindow()"></i>
+    @if (Auth::check())
+    <div style="pointer-events: all">
+    @else
+    <div style="pointer-events: none">
+    @endif
+        <i class="far fa-trash-alt" style="cursor: pointer" onclick="showWindow({{$seat_id}}, 'seats')"></i>
         <form action=/seats/{{ $seat_id ?? '' }}/save/ method="post">
             @csrf
             <select name="seat_type" class='edit-seat-type'>
