d6143e9328e7e02003d6efb472aefe58b96806c8
irynaosoba <ir.osoba@gmail.com>
2021-11-23 11:02:03 +0100

    change the calendar language to Norwegian, a date format and a style for the selected date.

--- resources/js/react_app.js
+++ resources/js/react_app.js
@@ -3,11 +3,18 @@ import ReactRenderer from './src/ReactRenderer'
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import nb from 'date-fns/locale/nb';
+registerLocale('nb', nb)
 
 const Example = () => {
     const [startDate, setStartDate] = useState(new Date());
     return (
-        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+        <DatePicker selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                    local={setDefaultLocale('nb')}
+                    dateFormat="d. MMMM"
+        />
     );
 };
 
