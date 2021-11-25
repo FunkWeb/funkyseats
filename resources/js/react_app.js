1b368e1e57b8c8823413aeb6e02c075f26e6f177
arevjensen <areveldejensen@gmail.com>
2021-11-25 10:47:33 +0100

    React for datetime picker

--- resources/js/react_app.js
+++ resources/js/react_app.js
@@ -3,25 +3,29 @@ import ReactRenderer from './src/ReactRenderer'
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
 import nb from 'date-fns/locale/nb';
 registerLocale('nb', nb)
 
 class Example extends React.Component {
-    changeDate(date) {  useState(new date())};
+    changeDate(date) { useState(new date()) };
 
     render() {
-        const changeDate = () => {
-            alert("hello");
+        const changeDate = (date) => {
+            let month = date.getMonth() + 1;
+            window.location.href = "/room/" + this.props.room_id + "/" + date.getDate() + "-" + month + "-" + date.getFullYear();
         };
-    return (
-        <div>
-            <DatePicker dateFormat="dd-M-yyyy"
-                        value={this.props.value}
-                        onChange={changeDate}/>
-        </div>
-    )
-}
+
+        return (
+            <div>
+                <DatePicker dateFormat="dd-M-yyyy"
+                    value={this.props.value}
+                    onChange={(date) => changeDate(date)}
+                    name="date_picker"
+                />
+            </div>
+        )
+    }
 }
 
 const components = [
