a3e42f993393241c1844b520b95f6fd60db287c5
Rolf-Arne Strandjord <83413670+Rolf85no@users.noreply.github.com>
2022-01-24 14:19:43 +0100

    Made objects of data, stats update.

--- resources/views/pages/admin/stats.blade.php
+++ resources/views/pages/admin/stats.blade.php
@@ -16,21 +16,21 @@
 
                         <div class="col">
                             <h5 class="mb-1">
-                                <span data-animation="number" data-value="50" id="highCount"></span>
+                                <span data-animation="number" data-value="" id="highCount"></span>
                             </h5>
                             <div> Seats booked high</div>
                         </div>
 
                         <div class="col">
                             <h5 class="mb-1">
-                                <span data-animation="number" data-value="37"> </span> 
+                                <span data-animation="number" data-value="" id="lowCount"> </span> 
                             </h5>
                             <div> Seats booked low </div>
                         </div>
 
                         <div class="col">
                             <h5 class="mb-1">
-                                <span data-animation="number" data-value="44"> </span>
+                                <span data-animation="number" data-value="" id="averageCount"> </span>
                             </h5>
                             <div> Seats booked average </div>
                         </div>
@@ -64,17 +64,165 @@
     <script defer src="/assets/plugins/d3/d3.min.js"></script>
     <script defer src="/assets/plugins/nvd3/build/nv.d3.min.js"></script>
     <script defer type='text/javascript'>
+          
+        let currentWeek = [
+            {
+                date:{
+                    day: 22,
+                    month: "jan",
+                    year: 2022,
+                },
+                bookings:{
+                    total: Math.floor(Math.random() * 50) + 10,
+                    halfDay: Math.floor(Math.random() * 30) + 5
+                }
+            },
+            {
+                date:{
+                    day: 23,
+                    month: "jan",
+                    year: 2022,
+                },
+                bookings:{
+                    total: Math.floor(Math.random() * 50) + 10,
+                    halfDay: Math.floor(Math.random() * 30) + 5
+                }
+            },
+            {
+                date:{
+                    day: 24,
+                    month: "jan",
+                    year: 2022,
+                },
+                bookings:{
+                    total: Math.floor(Math.random() * 50) + 10,
+                    halfDay: Math.floor(Math.random() * 30) + 5
+                }
+            },
+            {
+                date:{
+                    day: 25,
+                    month: "jan",
+                    year: 2022,
+                },
+                bookings:{
+                    total: Math.floor(Math.random() * 50) + 10,
+                    halfDay: Math.floor(Math.random() * 30) + 5
+                }
+            },
+            {
+                date:{
+                    day: 26,
+                    month: "jan",
+                    year: 2022,
+                },
+                bookings:{
+                    total: Math.floor(Math.random() * 50) + 10,
+                    halfDay: Math.floor(Math.random() * 30) + 5
+                }
+            }
+
+        ];
+
+
+        let lastWeek =  [{
+            date:{
+                day: 10,
+                month: "jan",
+                year: 2022,
+            },
+            bookings:{
+                total: 33,
+                halfDay: 22,
+            }
+        },
+        {
+            date:{
+                day: 16,
+                month: "jan",
+                year: 2022,
+            },
+            bookings:{
+                total: Math.floor(Math.random() * 50) + 10,
+                halfDay: Math.floor(Math.random() * 30) + 5
+            }
+        },
+        {
+            date:{
+                day: 17,
+                month: "jan",
+                year: 2022,
+            },
+            bookings:{
+                total: Math.floor(Math.random() * 50) + 10,
+                halfDay: Math.floor(Math.random() * 30) + 5
+            }
+        },
+        {
+            date:{
+                day: 18,
+                month: "jan",
+                year: 2022,
+            },
+            bookings:{
+                total: Math.floor(Math.random() * 50) + 10,
+                halfDay: Math.floor(Math.random() * 30) + 5
+            }
+        },
+        {
+            date:{
+                day: 19,
+                month: "jan",
+                year: 2022,
+            },
+            bookings:{
+                total: Math.floor(Math.random() * 50) + 10,
+                halfDay: Math.floor(Math.random() * 30) + 5
+            }
+        }
+
+    ];
+
+    currentWeek = JSON.stringify(currentWeek);
+    currentWeek = JSON.parse(currentWeek);
+
     let chartLoaded = false;
-    window.onload = function(){    
+    window.onload = function(){
         if (!chartLoaded){ 
         createGraph('current');
         }
         chartLoaded = true; 
     }
+
+
+    function highestVal(chartData){
+        const highCount = document.getElementById('highCount');
+        const lowCount = document.getElementById('lowCount');
+        const averageCount = document.getElementById('averageCount');
+        let maxValue = chartData[0].bookings.total;
+        let minValue = chartData[0].bookings.total;
+        let totalValue = 0;
+        for (let i=0; i <chartData.length; i++){
+            if (chartData[i].bookings.total > maxValue){
+                maxValue = chartData[i].bookings.total;
+            }
+            if (chartData[i].bookings.total < minValue){
+                minValue = chartData[i].bookings.total;
+            }
+            totalValue += chartData[i].bookings.total;
+        }
+
+        let avgValue = Math.round(totalValue / chartData.length); 
+        highCount.setAttribute('data-value', `${maxValue}`);
+        lowCount.setAttribute('data-value', `${minValue}`);
+        averageCount.setAttribute('data-value',`${avgValue}`);
+        highCount.innerHTML = maxValue;
+        lowCount.innerHTML = minValue;
+        averageCount.innerHTML = avgValue;
+        
+    }
         function createGraph(interval){
-            let today = 17;
-            let i;
-            let month = '.jan'
+
             function addGraph(data){
                 d3.select(".nvd3-svg").remove(); 
                 nv.addGraph({
@@ -87,114 +235,95 @@
                         return BarChart;
                     }    
                   });
-            }
-                
+            }                
                     if (interval === 'lastWeek'){
-                        i = 7;
                         var barChartDataLastW = [{
                             key: 'Total',
-                            'color' : '#20B3BE',
-                            values: [
-                                {x:`${today - i}${month}`, y:50},
-                                {x: `${today - i + 1}${month}`, y:44},                   
-                                {x:`${today - i + 2}${month}`, y:50},
-                                {x:`${today - i + 3}${month}`, y:41},                   
-                                {x:`${today - i + 4}${month}`, y:37}                   
-                            ]
-                          }, {
-                            key: 'Half day',
-                            color: '#324252',
-                            values: [
-                                {x:`${today - i}${month}`, y:25},
-                                {x:`${today - i + 1}${month}`, y:13},
-                                {x:`${today - i + 2}${month}`, y:11},
-                                {x:`${today - i + 3}${month}`, y:21},                   
-                                {x:`${today - i + 4}${month}`, y:9}    
-                            ]
-                          }];
-                          addGraph(barChartDataLastW);
-                   }
-
-                   else if (interval === 'month' || interval === 'lastMonth'){
-                    i = 16;
-                    var barChartDataM = [{
-                        key: 'Total',
                         'color' : '#20B3BE',
                         values: [
-                            {x:`1${month}`, y:50},
-                            {x:`2${month}`, y:44},                   
-                            {x:`3${month}`, y:50},
-                            {x: '4', y:41},                   
-                            {x: '5', y:37},                   
-                            {x:`8`, y:50},
-                            {x: '9', y:44},                   
-                            {x:'10', y:50},
-                            {x: '11', y:41},                   
-                            {x: '12', y:37},                   
-                            {x:`15`, y:50},
-                            {x: '16', y:44},                   
-                            {x:'17', y:50},
-                            {x: '18', y:41},                   
-                            {x: '19', y:37},                   
-                            {x:`22`, y:50},
-                            {x: '23', y:44},                   
-                            {x:'24', y:50},
-                            {x: '25', y:41},                   
-                            {x: '26', y:37}                   
+                        {x:`${lastWeek[0].date.day}.${lastWeek[0].date.month}`, y:`${lastWeek[0].bookings.total}`},
+                        {x:`${lastWeek[1].date.day}.${lastWeek[1].date.month}`, y:`${lastWeek[1].bookings.total}`},                
+                        {x:`${lastWeek[2].date.day}.${lastWeek[2].date.month}`, y:`${lastWeek[2].bookings.total}`},                
+                        {x:`${lastWeek[3].date.day}.${lastWeek[3].date.month}`, y:`${lastWeek[3].bookings.total}`},                
+                        {x:`${lastWeek[4].date.day}.${lastWeek[4].date.month}`, y:`${lastWeek[4].bookings.total}`},                
+                                         
                         ]
                       }, {
                         key: 'Half day',
                         color: '#324252',
                         values: [
-                        {x:`1${month}`, y:5},
-                        {x:`2${month}`, y:4},                   
-                        {x:`3${month}`, y:5},
-                        {x: '4', y:4},                   
-                        {x: '5', y:3},                   
-                        {x:`8`, y:5},
-                        {x: '9', y:4},                   
-                        {x:'10', y:5},
-                        {x: '11', y:4},                   
-                        {x: '12', y:3},                   
-                        {x:`15`, y:5},
-                        {x: '16', y:4},                   
-                        {x:'17', y:5},
-                        {x: '18', y:4},                   
-                        {x: '19', y:3},                   
-                        {x:`22`, y:5},
-                        {x: '23', y:4},                   
-                        {x:'24', y:5},
-                        {x: '25', y:4},                   
-                        {x: '26', y:3}     
+                        {x:`${lastWeek[0].date.day}.${lastWeek[0].date.month}`, y:`${lastWeek[0].bookings.halfDay}`},
+                        {x:`${lastWeek[1].date.day}.${lastWeek[1].date.month}`, y:`${lastWeek[1].bookings.halfDay}`},                
+                        {x:`${lastWeek[2].date.day}.${lastWeek[2].date.month}`, y:`${lastWeek[2].bookings.halfDay}`},                
+                        {x:`${lastWeek[3].date.day}.${lastWeek[3].date.month}`, y:`${lastWeek[3].bookings.halfDay}`},                
+                        {x:`${lastWeek[4].date.day}.${lastWeek[4].date.month}`, y:`${lastWeek[4].bookings.halfDay}`},      
+                        ]
+                          }];
+                          addGraph(barChartDataLastW);
+                          highestVal(lastWeek);
+                   }
+
+                   else if (interval === 'month' || interval === 'lastMonth'){
+                    var barChartDataM = [{
+                        key: 'Total',
+                        'color' : '#20B3BE',
+                        values: [
+                        {x:`${lastWeek[0].date.day}.${lastWeek[0].date.month}`, y:`${lastWeek[0].bookings.total}`},
+                        {x:`${lastWeek[1].date.day}.${lastWeek[1].date.month}`, y:`${lastWeek[1].bookings.total}`},                
+                        {x:`${lastWeek[2].date.day}.${lastWeek[2].date.month}`, y:`${lastWeek[2].bookings.total}`},                
+                        {x:`${lastWeek[3].date.day}.${lastWeek[3].date.month}`, y:`${lastWeek[3].bookings.total}`},                
+                        {x:`${lastWeek[4].date.day}.${lastWeek[4].date.month}`, y:`${lastWeek[4].bookings.total}`},
+                        {x:`${currentWeek[0].date.day}.${currentWeek[0].date.month}`, y:`${currentWeek[0].bookings.total}`},
+                        {x:`${currentWeek[1].date.day}.${currentWeek[1].date.month}`, y:`${currentWeek[1].bookings.total}`},                
+                        {x:`${currentWeek[2].date.day}.${currentWeek[2].date.month}`, y:`${currentWeek[2].bookings.total}`},                
+                        {x:`${currentWeek[3].date.day}.${currentWeek[3].date.month}`, y:`${currentWeek[3].bookings.total}`},                
+                        {x:`${currentWeek[4].date.day}.${currentWeek[4].date.month}`, y:`${currentWeek[4].bookings.total}`},   
+                        ]
+                      }, {
+                        key: 'Half day',
+                        color: '#324252',
+                        values: [
+                        {x:`${lastWeek[0].date.day}.${lastWeek[0].date.month}`, y:`${lastWeek[0].bookings.halfDay}`},
+                        {x:`${lastWeek[1].date.day}.${lastWeek[1].date.month}`, y:`${lastWeek[1].bookings.halfDay}`},                
+                        {x:`${lastWeek[2].date.day}.${lastWeek[2].date.month}`, y:`${lastWeek[2].bookings.halfDay}`},                
+                        {x:`${lastWeek[3].date.day}.${lastWeek[3].date.month}`, y:`${lastWeek[3].bookings.halfDay}`},                
+                        {x:`${lastWeek[4].date.day}.${lastWeek[4].date.month}`, y:`${lastWeek[4].bookings.halfDay}`},
+                        {x:`${currentWeek[0].date.day}.${currentWeek[0].date.month}`, y:`${currentWeek[0].bookings.halfDay}`},
+                        {x:`${currentWeek[1].date.day}.${currentWeek[1].date.month}`, y:`${currentWeek[1].bookings.halfDay}`},
+                        {x:`${currentWeek[2].date.day}.${currentWeek[2].date.month}`, y:`${currentWeek[2].bookings.halfDay}`},                
+                        {x:`${currentWeek[3].date.day}.${currentWeek[3].date.month}`, y:`${currentWeek[3].bookings.halfDay}`},                
+                        {x:`${currentWeek[4].date.day}.${currentWeek[4].date.month}`, y:`${currentWeek[4].bookings.halfDay}`},  
+                           
                         ]
                       }];
                       addGraph(barChartDataM);
 
                    }
                    else{
-                    i = 0;
                     var barChartData = [{
                         key: 'Total',
                         'color' : '#20B3BE',
                         values: [
-                            {x:`${today - i}${month}`, y:41},
-                            {x: `${today - i + 1}${month}`, y:44},                   
-                            {x:`${today - i + 2}${month}`, y:33},
-                            {x:`${today - i + 3}${month}`, y:37},                   
-                            {x:`${today - i + 4}${month}`, y:37}                   
+                        {x:`${currentWeek[0].date.day}.${currentWeek[0].date.month}`, y:`${currentWeek[0].bookings.total}`},
+                        {x:`${currentWeek[1].date.day}.${currentWeek[1].date.month}`, y:`${currentWeek[1].bookings.total}`},                
+                        {x:`${currentWeek[2].date.day}.${currentWeek[2].date.month}`, y:`${currentWeek[2].bookings.total}`},                
+                        {x:`${currentWeek[3].date.day}.${currentWeek[3].date.month}`, y:`${currentWeek[3].bookings.total}`},                
+                        {x:`${currentWeek[4].date.day}.${currentWeek[4].date.month}`, y:`${currentWeek[4].bookings.total}`},                
+                                         
                         ]
                       }, {
                         key: 'Half day',
                         color: '#324252',
                         values: [
-                            {x:`${today - i}${month}`, y:25},
-                            {x:`${today - i + 1}${month}`, y:13},
-                            {x:`${today - i + 2}${month}`, y:17},
-                            {x:`${today - i + 3}${month}`, y:21},                   
-                            {x:`${today - i + 4}${month}`, y:19}    
+                        {x:`${currentWeek[0].date.day}.${currentWeek[0].date.month}`, y:`${currentWeek[0].bookings.halfDay}`},
+                        {x:`${currentWeek[1].date.day}.${currentWeek[1].date.month}`, y:`${currentWeek[1].bookings.halfDay}`},
+                        {x:`${currentWeek[2].date.day}.${currentWeek[2].date.month}`, y:`${currentWeek[2].bookings.halfDay}`},                
+                        {x:`${currentWeek[3].date.day}.${currentWeek[3].date.month}`, y:`${currentWeek[3].bookings.halfDay}`},                
+                        {x:`${currentWeek[4].date.day}.${currentWeek[4].date.month}`, y:`${currentWeek[4].bookings.halfDay}`},     
                         ]
                       }];
                       addGraph(barChartData);
+                      highestVal(currentWeek);
                     }
                 }
 
