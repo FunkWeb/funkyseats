e669c055ea8e82d3c319d081545a9c49c7d899c5
Anders Pettersen <github@pecon.no>
2022-02-11 11:06:28 +0100

    Added names to most routes

--- routes/web.php
+++ routes/web.php
@@ -20,31 +20,31 @@ use App\Http\Controllers\SeatController;
 
 Route::group(['middleware' => 'role:admin'], function () {
     //Room admin routes
-    Route::get('/rooms/edit', [RoomController::class, 'edit']);
-    Route::post('/rooms/{id}/save', [RoomController::class, 'save']);
-    Route::post('/rooms/{id}/delete', [RoomController::class, 'delete']);
-    Route::post('/room/store', [RoomController::class, 'store']);
+    Route::get('/rooms/edit', [RoomController::class, 'edit'])->name('room.edit');
+    Route::post('/rooms/{id}/save', [RoomController::class, 'save'])->name('room.update');
+    Route::post('/rooms/{id}/delete', [RoomController::class, 'delete'])->name('room.destroy');
+    Route::post('/room/store', [RoomController::class, 'store'])->name('room.store');
 
     //Seat admin routes
-    Route::get('/room/{id}/seats/edit', [SeatController::class, 'edit']);
-    Route::post('/seats/{id}/save', [SeatController::class, 'save']);
-    Route::post('/seats/{id}/delete', [SeatController::class, 'delete']);
-    Route::post('/seat/store', [SeatController::class, 'store']);
+    Route::get('/room/{id}/seats/edit', [SeatController::class, 'edit'])->name('seat.edit');
+    Route::post('/seats/{id}/save', [SeatController::class, 'save'])->name('seat.update');
+    Route::post('/seats/{id}/delete', [SeatController::class, 'delete'])->name('seat.destroy');
+    Route::post('/seat/store', [SeatController::class, 'store'])->name('seat.store');
 });
 
 //Login routes
-Route::get('/auth/logout', [LogoutController::class, 'perform']);
+Route::get('/auth/logout', [LogoutController::class, 'perform'])->name('logout');
 Route::get('/auth/google', [GoogleController::class, 'googleRedirect'])->name('login');
-Route::get('/callback/google', [GoogleController::class, 'googleCallback']);
+Route::get('/callback/google', [GoogleController::class, 'googleCallback'])->name('google.callback');
 
 
 Route::group(['middleware' => 'auth'], function () {
     //Booking routes
-    Route::post('/booking/seat/{seat_id}', [BookingController::class, 'store']);
-    Route::post('/booking/seat/random/{room_id}', [BookingController::class, 'randomSeat']);
-    Route::get('/booking/delete/{booking_id}', [BookingController::class, 'delete'])->name('deleteBooking');
+    Route::post('/booking/seat/{seat_id}', [BookingController::class, 'store'])->name('booking.store');
+    Route::post('/booking/seat/random/{room_id}', [BookingController::class, 'randomSeat'])->name('booking.random');
+    Route::get('/booking/delete/{booking_id}', [BookingController::class, 'delete'])->name('booking.destroy');
 });
 
 Route::get('/', [RoomController::class, 'index_withCountSeats'])->name('home');
-Route::get('/display/{id}', [RoomController::class, 'show_display']);
-Route::get('/room/{id}/{datetime?}', [RoomController::class, 'show']);
+Route::get('/display/{id}', [RoomController::class, 'show_display'])->name('display.show');
+Route::get('/room/{id}/{datetime?}', [RoomController::class, 'show'])->name('room.show');
