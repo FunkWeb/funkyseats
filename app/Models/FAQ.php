cda331bf20a14cbd0a47dd571a1313a75064fcbc
arevjensen <areveldejensen@gmail.com>
2022-02-14 10:56:26 +0100

    Skeleton back-end faq data
    (cherry picked from commit 0bae8b363d3aa6d175225693961b6c64ffe1cced)

---
+++ app/Models/FAQ.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FAQ extends Model
+{
+    use HasFactory;
+
+    protected $attributes = [
+        'question_order' => 0,
+    ];
+}
