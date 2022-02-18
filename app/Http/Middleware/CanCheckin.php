74256374fcf4b0d1f6bb5aad3e5c8f2923a4efa4
arevjensen <areveldejensen@gmail.com>
2022-02-18 10:19:41 +0100

    $request->ip does not give correct ip

--- app/Http/Middleware/CanCheckin.php
+++ app/Http/Middleware/CanCheckin.php
@@ -16,7 +16,7 @@ class CanCheckin
      */
     public function handle(Request $request, Closure $next)
     {
-        if ($request->ip == config('checkin.checkin_ip') || $request->ip == "127.0.0.1") {
+        if (\Request::ip() == config('checkin.checkin_ip') || $request->ip == "127.0.0.1") {
             return $next($request);
         }
 
