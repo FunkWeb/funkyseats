4ae48edfc3234c5c57ed92754d4e3fe1125cd285
Rolf-Arne Strandjord <83413670+Rolf85no@users.noreply.github.com>
2022-02-24 14:26:00 +0100

    small design changes

--- resources/views/pages/admin/role_assignment.blade.php
+++ resources/views/pages/admin/role_assignment.blade.php
@@ -17,63 +17,63 @@
             </div>
         </div>
         <div class="panel-body p-0 roles_stats">
-                <h3 > Navn navnesen</h5>
+                <h3 > Navn Navnesen</h5>
                 <div class="row">
                     <div class="col-6">
-                        <h5> Hours this month:  </h5>
+                        <h5> Hours this month: 70</h5>
                     </div>
                     <div class="col-6">
-                        <h5> Hours this week: </h5>
+                        <h5> Hours this week: 20</h5>
                     </div>
 
                 <hr>
                 <div class="row">
                     <div class="col">
-                        <h5> Mon: </h5>
+                        <h5> Mon: 2</h5>
                     </div>
                     <div class="col">
-                        <h5> Tue: </h5>
+                        <h5> Tue: 2</h5>
                     </div>
                     <div class="col">
-                        <h5> Wed: </h5>
+                        <h5> Wed: 2</h5>
                     </div>
                     <div class="col">
-                        <h5> Thu: </h5>
+                        <h5> Thu: 1</h5>
                     </div>
                     <div class="col">
-                        <h5> Fri: </h5>
+                        <h5> Fri: 2</h5>
                     </div>
                 </div>
                     
                 </div>
 
                 <hr>
-                <div class="row roles_buttons">
+                <div class="row roles_row">
                     
-                    <div class="col"> <h5> Add Role: </h5> </div> 
-                    <div class="col">
-                            <button class="submit-changes-btn roles_btn"> Veileder </button>
+                    <div class="col-4 col-md-2"> <h5> Add Role: </h5> </div> 
+                    <div class="col-4 col-md-2">
+                            <button class="submit-changes-btn roles_btn"> Veileder <i class="fas fa-plus"></i></button>
                     </div>
-                    <div class="col">
-                            <button class="submit-changes-btn roles_btn">Admin</button>
+                    <div class="col-4 col-md-2">
+                            <button class="submit-changes-btn roles_btn">Admin <i class="fas fa-plus"></i></button>
                     </div>
 
-                    <div class="col"> <h5> Remove Role: </h5> </div> 
-                    <div class="col">
-                            <button class="submit-changes-btn roles_btn remove">Veileder </button>
+                    <div class="col-4 col-md-2"> <h5> Remove Role: </h5> </div> 
+                    <div class="col-4 col-md-2">
+                            <button class="submit-changes-btn roles_btn remove">Veileder <i class="fas fa-minus"></i> </button>
                     </div>
-                    <div class="col">
-                            <button class="submit-changes-btn roles_btn remove">Admin</button>
+                    <div class="col-4 col-md-2">
+                            <button class="submit-changes-btn roles_btn remove">Admin <i class="fas fa-minus"></i></button>
                     </div>
                 </div> 
                 
                 <hr>
                 <div class="row delete_buttons">
                     <div class="col">
-                        <button class="submit-changes-btn delMake">Delete candidate </button>
+                        <button class="submit-changes-btn delMake_btn">Delete <i class="fas fa-trash"></i></button>
                     </div>
                     <div class="col">
-                        <button class="submit-changes-btn delMake">Make anonymous </button>
+                        <button class="submit-changes-btn delMake_btn">Anonymize <i class="fas fa-user-secret"></i></button>
                     </div>
                 </div>
         </div> 
@@ -83,51 +83,5 @@
     <link href="../assets/plugins/tag-it/css/jquery.tagit.css" rel="stylesheet" />
     <script defer src="../assets/plugins/jquery-migrate/dist/jquery-migrate.min.js"></script>
     <script defer src="../assets/plugins/tag-it/js/tag-it.min.js"></script>
-    <script defer type='text/javascript'>
-
-        const personJSON = `
-        [
-            {
-                "name": "Jens",
-                "name_id": "0",
-                "assigned_roles":{
-                    "role_name":["Veileder", "Admin"],
-                    "role_id": ["0", "1"]
-                    },
-                "unassigned_roles":{
-                    "role_name":["Leder"],
-                    "role_id": ["2"]
-                    }
-            },
-            {
-                "name": "Jakob",
-                "name_id": "1",
-                "assigned_roles":{
-                    "role_name":["Veileder"],
-                    "role_id": ["0"]
-                    },
-                 "unassigned_roles":{
-                    "role_name":["Admin","Leder"],
-                    "role_id": ["1","2"]
-                    }
-            },
-            {
-                "name": "Silje",
-                "name_id": "2",
-                "assigned_roles":{
-                    "role_name":["Admin"],
-                    "role_id": ["1"]
-                    },
-                "unassigned_roles":{
-                    "role_name":["Veilder","Leder"],
-                    "role_id": ["0","2"]
-                    }
-            }
-        ]`
-        
-        let parsedPerson = JSON.parse(personJSON);
-        
-        
-</script> 
 
 @endsection
