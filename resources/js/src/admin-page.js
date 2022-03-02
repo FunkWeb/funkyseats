1db047b1f5bdbb764dde4ae1d49570fd23f6da97
Rolf-Arne Strandjord <83413670+Rolf85no@users.noreply.github.com>
2022-03-02 10:13:48 +0100

    test 2

--- resources/js/src/admin-page.js
+++ resources/js/src/admin-page.js
@@ -105,7 +105,7 @@ function addNewSeatType(){
             </div>
             
         </div>
-        <div class="row">
+        <div class="row new-seat-type-row">
             <div class="col-4">
                 <label for ="seat-type-name">Seat-name</label>
                 <input type="text" class="form-control" id="seat-type-name" name="name" placeholder="Write seat name">
@@ -118,7 +118,7 @@ function addNewSeatType(){
         </div>
                             
             <div class="row edit_seat_type_btns_row">
-                    <button class='submit-changes-btn seat_type_btn' type='submit' value='submit'>Save</button>
+                    <button class='submit-changes-btn seat_type_btn newseat' type='submit' value='submit'>Save</button>
             </div> 
     </form>
 </div> `
