73b63747c8741403c5169d69bf7e948ffd35f4f4
Rolf-Arne Strandjord <83413670+Rolf85no@users.noreply.github.com>
2022-03-11 10:28:48 +0100

    Changed header based on role user profile-page

--- resources/views/pages/admin/user_profile.blade.php
+++ resources/views/pages/admin/user_profile.blade.php
@@ -7,7 +7,11 @@
 @section('content')
  <div class="panel panel-inverse">
         <div class="panel-heading ui-sortable handle">
+        @if(Auth::user()->hasRole('admin'))
             <h3 class="panel-title"> Roles and stats</h3>
+        @else
+            <h3 class="panel-title"> Stats</h3>
+        @endif
             <div class="panel-heading-btn">
                 <a href="javascript:;" class="btn btn-xs btn-icon btn-default" data-toggle="panel-expand"><i class="fa fa-expand"></i></a>
                 <a href="javascript:;" class="btn btn-xs btn-icon btn-success" data-toggle="panel-reload"><i class="fa fa-redo"></i></a>
