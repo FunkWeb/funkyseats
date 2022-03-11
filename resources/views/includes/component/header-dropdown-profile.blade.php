6c6f221e8098623bdfd7856a985f08ef71f112de
Rolf-Arne Strandjord <83413670+Rolf85no@users.noreply.github.com>
2022-03-11 12:11:14 +0100

    removed edit profile from dropdown

--- resources/views/includes/component/header-dropdown-profile.blade.php
+++ resources/views/includes/component/header-dropdown-profile.blade.php
@@ -1,5 +1,5 @@
 <div class="dropdown-menu dropdown-menu-end">
-	<a href="javascript:" class="dropdown-item">Edit Profile</a>
+	{{-- <a href="javascript:" class="dropdown-item">Edit Profile</a> --}}
 	<a href="/profile/{{Auth::user()->id}}" class="dropdown-item">My profile</a>
 	@can('checkin-ip')
 		@if(Auth::user()->getCheckedInAttribute() == 1)
@@ -8,9 +8,9 @@
 		<a href="/checkin" class="dropdown-item">Check In</a>
 		@endif
 	@endcan
-	<!-- <a href="javascript:;" class="dropdown-item"><span class="badge bg-danger float-end rounded-pill">2</span> Inbox</a>
+	{{-- <a href="javascript:;" class="dropdown-item"><span class="badge bg-danger float-end rounded-pill">2</span> Inbox</a>
 	<a href="javascript:;" class="dropdown-item">Calendar</a>
-	<a href="javascript:;" class="dropdown-item">Setting</a>-->
+	<a href="javascript:;" class="dropdown-item">Setting</a>--}}
 	<div class="dropdown-divider"></div>
 	<a href="/auth/logout" class="dropdown-item">Log Out</a>
 </div>
