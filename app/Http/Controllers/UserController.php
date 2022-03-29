22e91105ebb9ac20ad9f0a45828a826653c444cc
arevjensen <areveldejensen@gmail.com>
2022-03-29 09:33:18 +0200

    Refactor of user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,37 +20,15 @@ class UserController extends Controller
     {
         //Using route model binding locks into load() if we want one object with all data
         $userData = $user
-            //->checkins()->select(DB::raw('SUM(TIMESTAMPDIFF(MINUTE,created_at,checkout_at)) as total'),
-            // DB::raw('SUM(case when YEARWEEK(`created_at`, 1) = YEARWEEK(CURDATE(), 1) 
-            //   then TIMESTAMPDIFF(MINUTE,created_at,checkout_at) 
-            //   else 0 end) as week'),
-            // DB::raw('SUM(forced_checkout) as forced'),
-            // DB::raw('SUM(case when WEEKDAY(created_at)=0 then 1 else 0 end) as Monday'),
-            // DB::raw('SUM(case when WEEKDAY(created_at)=1 then 1 else 0 end) as Tuesday'),
-            // DB::raw('SUM(case when WEEKDAY(created_at)=2 then 1 else 0 end) as Wednesday'),
-            // DB::raw('SUM(case when WEEKDAY(created_at)=3 then 1 else 0 end) as Thursday'),
-            // DB::raw('SUM(case when WEEKDAY(created_at)=4 then 1 else 0 end) as Friday'))
-            ->load('roles'); // or ->roles() <- this gives no user model data only the roles
-        //->load('checkins');
+            ->load('roles');
 
         $other_roles = Role::whereDoesntHave('user', function ($q) use ($user) {
             $q
                 ->where('id', $user->id);
         })->get();
 
-        //Wheremonth can be changed later if they want spesific month
-        $checkins = Checkin::select(
-            DB::raw('SUM(TIMESTAMPDIFF(MINUTE,created_at,checkout_at)) as total'),
-            DB::raw('SUM(case when YEARWEEK(`created_at`, 1) = YEARWEEK(CURDATE(), 1) 
-              then TIMESTAMPDIFF(MINUTE,created_at,checkout_at) 
-              else 0 end) as week'),
-            DB::raw('SUM(forced_checkout) as forced'),
-            DB::raw('SUM(case when WEEKDAY(created_at)=0 then 1 else 0 end) as Monday'),
-            DB::raw('SUM(case when WEEKDAY(created_at)=1 then 1 else 0 end) as Tuesday'),
-            DB::raw('SUM(case when WEEKDAY(created_at)=2 then 1 else 0 end) as Wednesday'),
-            DB::raw('SUM(case when WEEKDAY(created_at)=3 then 1 else 0 end) as Thursday'),
-            DB::raw('SUM(case when WEEKDAY(created_at)=4 then 1 else 0 end) as Friday')
-        )->where('user_id', $user->id)->whereMonth('created_at', date('m'))->get()->first();
+        //userCheckInStats just gets current month, can be updated to take custom month
+        $checkins = Checkin::userCheckinStats($user->id);
 
         $checkins->total =  intdiv($checkins->total, 60) . ':' . ($checkins->total % 60);
         $checkins->week =  intdiv($checkins->week, 60) . ':' . ($checkins->week % 60);
