d6845d4095eb4bcb7b217dfac8fc7ebac7d7645b
arevjensen <areveldejensen@gmail.com>
2022-03-30 11:51:02 +0200

    removed autogenerated code

--- app/Policies/BookingPolicy.php
+++ app/Policies/BookingPolicy.php
@@ -12,52 +12,6 @@ class BookingPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Booking  $booking
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, Booking $booking)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Booking  $booking
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function update(User $user, Booking $booking)
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can delete the model.
      *
      * @param  \App\Models\User  $user
@@ -73,29 +27,5 @@ class BookingPolicy
         } else {
             return false;
         }
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Booking  $booking
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, Booking $booking)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Booking  $booking
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, Booking $booking)
-    {
-        //
     }
 }
