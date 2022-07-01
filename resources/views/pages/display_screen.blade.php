c8d4c8f6fa384ef6cefc3f66f87dacb29dca395b
Rolf-Arne Strandjord <83413670+Rolf85no@users.noreply.github.com>
2022-07-01 15:44:29 +0200

    Display page working with seeded data.

--- resources/views/pages/display_screen.blade.php
+++ resources/views/pages/display_screen.blade.php
@@ -1,84 +1,87 @@
-@component('includes.head')
-@endcomponent
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$room[0]->name}}</title>
+    <meta http-equiv="refresh" content="600">
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
 
 <script>
-    function refreshPage(refreshRate) {
-        setTimeout("location.reload(true);", refreshRate);
+    function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        h = checkTime(h);
+        m = checkTime(m);
+        document.getElementById('time').innerHTML =
+            h + ":" + m;
+        var t = setTimeout(startTime, 1000);
     }
 
-    window.onload = refreshPage(5000 * 60);
+    function checkTime(i) {
+        if (i < 10) {
+            i = "0" + i
+        }
+        ;  // add zero in front of numbers < 10
+        return i;
+    }
+    
 </script>
 
-{{-- {{ ddd($room[0]->seat[0]->booking[0]->user) }} --}}
-<div class="panel panel-inverse" data-sortable-id="table-basic-1">
+<body class="py-4 px-20 bg-indigo-50" onload="startTime()">
+    {{-- {{ddd($room[0]->seat)}} --}}
+<div class="flex justify-between py-12 text-4xl font-bold text-center text-gray-700">
+    <div>{{$room[0]->name}}</div>
+    <div id="time"></div>
+</div>
 
-    <div class="panel-heading ui-sortable-handle" style="justify-content:center;">
-        <h4>{{ $room[0]->name }} - {{ \Carbon\Carbon::now()->isoFormat('Do MMM Y') }}</h4>
-    </div>
-
-    <div class="panel-body">
-
-        <div class="table-responsive">
-            <table class="table mb-0">
-                <thead>
-                    <tr>
-                        <th>Seat</th>
-                        <th>Booked before lunch</th>
-                        <th>Booked after lunch</th>
-                        <th>Check in status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($room[0]->seat as $seat)
-                        <tr>
-                            <td>{{ $seat->seat_number }}</td>
-                            @if (count($seat->booking) == 0)
-                                <td></td>
-                                <td></td>
-                                <td></td>
+<div class="relative overflow-x-auto shadow-2xl sm:rounded-lg">
+    <table class="w-full text-2xl text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xl text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+            <th scope="col" class="px-6 py-3">Plass nr</th>
+            <th scope="col" class="px-6 py-3">Person</th>
+            <th scope="col" class="px-6 py-3">Status</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($room[0]->seat as $seat)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-2 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                    {{ $seat->seat_number}}
+                </th>
+                 @if(count($seat->booking) !== 0)
+                    @foreach($seat->booking as $booking)
+                        @if(\Carbon\Carbon::parse ($booking->from)->format('H') < \Carbon\Carbon::now()->format('H') 
+                            && \Carbon\Carbon::parse ($booking->to)->format('H') > \Carbon\Carbon::now()->format('H'))
+                        <td class="px-6 py-2">
+                            {{ $booking->user->given_name }}
+                        </td>
+                        <td class="px-6 py-2 font-bold">
+                            @if($booking->checkin)
+                            <span class="text-emerald-400">Sjekket inn</span>
                             @else
-                                {{-- {{ ddd($seat->booking[0]) }} --}}
-                                @foreach ($seat->booking as $booking)
-                                    @if (\Carbon\Carbon::parse($booking->from)->format('H') == 8 && \Carbon\Carbon::parse($booking->to)->format('H') == 16)
-                                        <td>{{ $booking->user->given_name ?? '' }}</td>
-                                        <td>{{ $booking->user->given_name ?? '' }}</td>
-                                        @if ($booking->checkin)
-                                            <td> Checked in </td>
-                                        @else
-                                            <td> Not checked in </td>
-                                        @endif
-                                    @elseif(\Carbon\Carbon::parse($booking->from)->format('H') == 8)
-                                        @if (\Carbon\Carbon::parse($booking->from)->format('H') == 12)
-                                            <td>{{ $booking->user->given_name ?? '' }}</td>
-                                            <td>{{ $booking->user->given_name ?? '' }}</td>
-                                            @if ($booking->checkin)
-                                                <td> Checked in </td>
-                                            @else
-                                                <td> Not checked in </td>
-                                            @endif
-                                        @else
-                                            <td>{{ $booking->user->given_name ?? '' }}</td>
-                                            <td></td>
-                                            @if ($booking->checkin)
-                                                <td> Checked in </td>
-                                            @else
-                                                <td> Not checked in </td>
-                                            @endif
-                                        @endif
-                                    @elseif (\Carbon\Carbon::parse($booking->from)->format('H') == 12)
-                                        <td></td>
-                                        <td>{{ $booking->user->given_name ?? '' }}</td>
-                                        @if ($booking->checkin)
-                                            <td> Checked in </td>
-                                        @else
-                                            <td> Not checked in </td>
-                                        @endif
-                                    @endif
-                                @endforeach
+                            <span class="text-rose-600">Ikke tilstede</span>
                             @endif
-
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                        </td>
+                        @endif
+                    @endforeach    
+                @else
+                    <td class="px-6 py-2">
+                    </td>
+                    <td class="px-6 py-2 font-bold">Ledig </td>
+                 @endif
+               
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
